34357b665ea0c7f67e6ef7f40f1b3fa185bc9a92
Kkenize <Kkenizez@gmail.com>
2025-01-19 18:44:53 -0500

    Homework Practice Two of Week One! The new Challenge.

--- HomeworkPracticeTwoWeekOne/ContentView.swift
+++ HomeworkPracticeTwoWeekOne/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image = ""
+    @State private var message = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'bout")
+                .font(.largeTitle)
+                .bold()
+                .foregroundStyle(.purple)
+                .frame(width:300, height:100)
+            
+            Spacer()
+            
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    image = "peacesign"
+                    message = "Peace"
+                }
+                Button("Love") {
+                    image = "heart"
+                    message = "Love"
+                }
+                Button("Understanding") {
+                    image = "lightbulb"
+                    message = "Understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
