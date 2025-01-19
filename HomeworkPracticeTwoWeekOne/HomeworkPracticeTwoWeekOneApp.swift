a0ea0b4831160f073f71ba610845d747de708e73
Kkenize <Kkenizez@gmail.com>
2025-01-19 18:19:24 -0500

    Initial Commit

---
+++ HomeworkPracticeTwoWeekOne/HomeworkPracticeTwoWeekOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkPracticeTwoWeekOneApp.swift
+//  HomeworkPracticeTwoWeekOne
+//
+//  Created by Zhejun Zhang on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkPracticeTwoWeekOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
